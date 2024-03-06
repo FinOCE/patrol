1d28406f4c4cf84ee7f8adff62cc95011ebe1634
Fin <me@itsf.in>
2024-03-06 11:58:24 +1000

    Add catch to about command reply

--- src/index.ts
+++ src/index.ts
@@ -99,7 +99,7 @@ client.on("interactionCreate", async interaction => {
 
   // Handle about me
   if (interaction.isChatInputCommand() && interaction.commandName === "about") {
-    await interaction.reply({ embeds: [AboutEmbed()], ephemeral: true })
+    await interaction.reply({ embeds: [AboutEmbed()], ephemeral: true }).catch(console.error)
     return
   }
 
